cd2617429b115c34d279ef3f61d104368db9951b
darkalbatrus <dark.albatrus@gmail.com>
2025-07-10 09:43:42 +0330

    Edit Subscriber Section And Connect to db to save

--- include/layout/sidebar.php
+++ include/layout/sidebar.php
@@ -44,30 +44,55 @@ $categories = $db->query("SELECT * FROM categories");
     </ul>
   </div>
 
+  <?php
+  $invalidInputName = '';
+  $invalidInputEmail = '';
+
+  if (isset($_POST['subscribe'])) {
+    if (empty(trim($_POST['name']))) {
+      $invalidInputName = "فید نام الزامی است";
+    } elseif (empty(trim($_POST['email']))) {
+      $invalidInputEmail = "فید ایمیل الزامی است";
+    } else {
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $subscribeInsert = $db->prepare("INSERT INTO subscribers (name,email) VALUES (:name,:email)");
+      $subscribeInsert->execute(['name' => $name, 'email' => $email]);
+
+      $successMessage = "عضویت شما با موفقیت انجام شد";
+    }
+  }
+  ?>
   <!-- Subscribue Section -->
   <div class="card mt-4">
     <div class="card-body">
-      <p class="fw-bold fs-6">عضویت در خبرنامه</p
-        <form>
-      <div class="mb-3">
-        <label class="form-label">نام</label>
-        <input
-          type="text"
-          class="form-control" />
-      </div>
-      <div class="mb-3">
-        <label class="form-label">ایمیل</label>
-        <input
-          type="email"
-          class="form-control" />
-      </div>
-      <div class="d-grid gap-2">
-        <button
-          type="submit"
-          class="btn btn-secondary">
-          ارسال
-        </button>
-      </div>
+      <p class="fw-bold fs-6">عضویت در خبرنامه</p>
+      <form method="post">
+        <div class="mb-3">
+          <label class="form-label">نام</label>
+          <input
+            type="text"
+            name="name"
+            class="form-control" />
+          <div class="text-danger form-text"><?= $invalidInputName ?></div>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">ایمیل</label>
+          <input
+            type="email"
+            name="email"
+            class="form-control" />
+          <div class="text-danger form-text"><?= $invalidInputEmail ?></div>
+        </div>
+        <div class="d-grid gap-2">
+          <button
+            name="subscribe"
+            type="submit"
+            class="btn btn-secondary">
+            ارسال
+          </button>
+          <div class="text-success form-text"><?= $successMessage ?></div>
+        </div>
       </form>
     </div>
   </div>
