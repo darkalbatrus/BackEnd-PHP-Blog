9b6c23c96237ade795d0aac7178d5a5dc4fc8e23
darkalbatrus <dark.albatrus@gmail.com>
2025-07-15 00:30:46 +0330

    Optimize Slider in Index Page

--- include/layout/slider.php
+++ include/layout/slider.php
@@ -1,10 +1,7 @@
 <?php
-
 $sliders = $db->query("SELECT * FROM posts_slider");
-
 ?>
 
-<!-- Slider Section -->
 <section>
   <div id="carousel" class="carousel slide">
     <div class="carousel-indicators">
@@ -18,19 +15,24 @@ $sliders = $db->query("SELECT * FROM posts_slider");
         <?php foreach ($sliders as $slider) : ?>
           <?php
           $postId = $slider['post_id'];
-          $post = $db->query("SELECT * FROM posts WHERE id = $postId")->fetch();
+          $postQuery = $db->prepare("SELECT * FROM posts WHERE id = :id");
+          $postQuery->execute(['id' => $postId]);
+          $post = $postQuery->fetch();
+
+          $imgId = $post['file_id'];
+          $imageQuery = $db->prepare("SELECT name FROM files WHERE id = :id");
+          $imageQuery->execute(['id' => $imgId]);
+          $imgName = $imageQuery->fetchColumn();
           ?>
-          <div class="carousel-item overlay carousel-height <?= ($slider['active']) ? 'active' : '' ?>">
-            <img src="./uploads/posts/<?= $post['image'] ?>" class="d-block w-100" alt="post-image" />
+          <div class="carousel-item overlay carousel-height <?= $slider['active'] ? 'active' : '' ?>">
+            <img src="./uploads/posts/<?= htmlspecialchars($imgName) ?>" class="d-block w-100" alt="post-image" />
             <div class="carousel-caption d-none d-md-block">
-              <h5><?= $post['title'] ?></h5>
-              <p>
-                <?= substr($post['body'], 0, 200) . "..." ?>
-              </p>
+              <h5><?= htmlspecialchars($post['title']) ?></h5>
+              <p><?= htmlspecialchars(substr($post['body'], 0, 200)) ?>...</p>
             </div>
           </div>
-        <?php endforeach ?>
-      <?php endif ?>
+        <?php endforeach; ?>
+      <?php endif; ?>
     </div>
 
     <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
